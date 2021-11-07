367d085ecd0098f5ad53c65db13ade9f7136dc48
auttij1 <juhana.autti@gmail.com>
2021-11-07 15:20:35 +0200

    basic implementation of customer app

--- exercise/customer_app.py
+++ exercise/customer_app.py
@@ -22,14 +22,34 @@ class CustomerEventConsumer:
 
     def initialize_rabbitmq(self):
         # To implement - Initialize the RabbitMq connection, channel, exchange and queue here
+        
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters('localhost'))
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange='customer_app_events', exchange_type='topic')
+        result = self.channel.queue_declare('', exclusive=True)
+        self.temporary_queue_name = result.method.queue
+        self.channel.queue_bind(exchange='customer_app_events', queue=self.temporary_queue_name, routing_key=self.customer_id)
+
         xprint("CustomerEventConsumer {}: initialize_rabbitmq() called".format(self.customer_id))
 
     def handle_event(self, ch, method, properties, body):
         # To implement - This is the callback that is passed to "on_message_callback" when a message is received
+        event = json.loads(body.decode())
+        if 'event_type' in event:
+            shopping_event = ProductEvent(event['even_type'], event['product_number'], event['timestamp'])
+            self.shopping_events.append(shopping_event)
+        else:
+            billing_event = BillingEvent(event['customer_id'], event['product_number'], event['pickup_time'], event['purchase_time'], event['shopping_cost'])
+            self.billing_events.append(billing_event)
+        
+        self.channel.basic_ack(delivery_tag=method.delivery_tag)
         xprint("CustomerEventConsumer {}: handle_event() called".format(self.customer_id))
 
     def start_consuming(self):
         # To implement - Start consuming from Rabbit
+        self.channel.basic_consume(queue=self.temporary_queue_name, on_message_callback=self.handle_event)
+        self.channel.start_consuming()
         xprint("CustomerEventConsumer {}: start_consuming() called".format(self.customer_id))
 
     def close(self):
@@ -44,5 +64,5 @@ class CustomerEventConsumer:
                 self.connection.close()
         except Exception as e:
             print("CustomerEventConsumer {}: Exception {} on close()"
-                  .format(self.customer_id, e))
+                    .format(self.customer_id, e))
             pass
